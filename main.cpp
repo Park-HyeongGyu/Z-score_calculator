fdc716d26ce09a9212739b77b97a6ae242478daa
Park-HyeongGyu <phg7889@gmail.com>
2021-07-22 22:53:11 +0900

    Initial commit_2021-0722

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <cmath> // double pow(double base, double exponent), double sqrt(double x)
+#include "polynominal.hpp"
+
+const double pi = 3.141592653589793;
+using namespace std;
+
+vector<double> getCoefs(const unsigned int);
+unsigned int factorial(const unsigned int);
+
+int main()
+{
+    polynominal *pdf = new polynominal;
+    int num = 1;
+    cout << "Input how many terms to approximate. When input 0, loop ends. : ";
+    cin >> num;
+    pdf->setCoefficients(getCoefs(num));
+    cout << "Probability density function which is approximated with taylor series." << endl;
+    pdf->printPolynominal();
+    cout << endl;
+
+    double z_score = 0;
+    while(true)
+    {
+        cout<< "Input z-score(Loop ends when -1 is inputed.) : ";
+        cin >> z_score;
+        if(z_score == -1)
+        {
+            break;
+        }
+        cout << "P(0<Z<z-score) = " << pdf->getDefiniteIntegral(0, z_score) << endl;
+    }
+
+	delete pdf;
+    return 0;
+}
+
+vector<double> getCoefs(const unsigned int num) // get coefficients of taylor series of {1/sprt(2pi)}*e^(-x^2/2)
+{
+    unsigned int goal_degree = (num-1) * 2;
+    vector<double> coefs;
+
+    for(unsigned int degree = 0 ; degree <= goal_degree ; degree++)
+    {
+        if(degree % 2 == 1)
+        {
+            coefs.push_back(0);
+            continue;
+        }
+        unsigned int n = degree / 2;
+        double coef = 1/sqrt(2*pi);
+        double a = pow(-1.0, static_cast<double>(n));
+        double b = pow(2.0, static_cast<double>(n));
+        double c = factorial(n);
+        
+        coefs.push_back(coef*a/(b*c));
+    }
+
+    return coefs;
+}
+
+unsigned int factorial(const unsigned int num)
+{
+    unsigned int result = 1;
+    for(unsigned int count=1 ; count <= num ; count++)
+    {
+        result *= count;
+    }
+    return result;
+}
+
---
+++ polynominal.hpp
@@ -0,0 +1,126 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <cmath> // double pow(double base, double exponent)
+
+using namespace std;
+
+class polynominal{
+  private:
+    vector<double> coefficients;
+
+  public:
+    void setCoefficient(const int degree, const double coefficient)
+    {
+        if(coefficients.size() < degree+1)
+        {
+            coefficients.resize(degree+1);
+            coefficients.at(degree) = coefficient;
+        }
+        else
+        {
+            coefficients.at(degree) = coefficient;
+        }
+    }
+
+    void setCoefficients(const vector<double> original_coefficients)
+    {
+        coefficients.clear();
+        for(auto x : original_coefficients)
+        {
+            coefficients.push_back(x);
+        }
+    }
+
+    double getValue(const double x)
+    {
+        double result = 0;
+        for(int degree = 0 ; degree < coefficients.size() ; degree++)
+        {
+            result += coefficients.at(degree) * pow(x, static_cast<double>(degree));
+        }
+        return result;
+    }
+
+
+    polynominal* getDerivative()
+    {
+        polynominal *derivative = new polynominal;
+        for(int degree = 0 ; degree < coefficients.size()-1 ; degree++)
+        {
+            derivative->setCoefficient(degree, coefficients.at(degree+1)*(degree+1));
+        }
+        return derivative;
+    }
+
+    polynominal* getIndefiniteIntegral()
+    {
+        // constatnt of integral will be 0
+        polynominal *indefinite_integral = new polynominal;
+        indefinite_integral->setCoefficient(0, 0);
+        for(int degree = 0 ; degree < coefficients.size() ; degree++)
+        {
+            indefinite_integral->setCoefficient(degree+1, coefficients.at(degree)/(degree+1));
+        }
+        return indefinite_integral;
+    }
+
+    double getDefiniteIntegral(const double from, const double to) // F(to) - F(from)
+    {
+        polynominal *indefinite_integral = getIndefiniteIntegral();
+        double F_from = indefinite_integral->getValue(from);
+        double F_to = indefinite_integral->getValue(to);
+        double result = F_to - F_from;
+
+        delete indefinite_integral;
+        return result;
+    }
+    // TO DO, TO MODIFY : Because I don't know what are 위끝 and 아래끝 in english, I just write them as to, from. Modify it to proper term!
+
+
+    void printCoefficients()
+    {
+        for(auto x : coefficients)
+        {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
+    void printPolynominal()
+    {
+        for(int degree=0 ; degree < coefficients.size() ; degree++)
+        {
+            if(degree == 0)
+            {
+                cout << coefficients.at(degree);
+            }
+            else
+            {
+                if(coefficients.at(degree) == -1)
+                {
+                    cout << " - " << "x^" << degree;
+                }
+                else if(coefficients.at(degree) == 1)
+                {
+                    cout << " + " << "x^" << degree;
+                }
+                else if(coefficients.at(degree) == 0)
+                {
+                    continue;
+                }
+                else if(coefficients.at(degree) < 0 && coefficients.at(degree) != -1)
+                {
+                    cout << " - " << (-1)*coefficients.at(degree) << "x^" << degree;
+                }
+                else if (coefficients.at(degree) > 0 && coefficients.at(degree) != 1)
+                {
+                    cout << " + " << coefficients.at(degree) << "x^" << degree;
+                }
+            }
+        }
+        cout << endl;
+    }
+};
+
